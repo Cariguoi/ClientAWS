a3cabcb30ce16d9d0b4564fab684a64fa409c56c
Cariguoi <43266894+Cariguoi@users.noreply.github.com>
2022-03-05 15:46:17 +0100

    request

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
+import requests
 from flask import Flask, render_template, request, url_for, flash, redirect
-from werkzeug.exceptions import abort
+import settings
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'  # à renseigner
@@ -11,23 +11,29 @@ def index():
     return render_template('index.html')
 
 
-def get_db_connection():
-    conn = sqlite3.connect('database.db')
-    conn.row_factory = sqlite3.Row
-    return conn
+def post_rds(classes, teacherName, hoursNumber, description, document, storages):
+    data = {'class': classes, 'teacherName': teacherName, 'hoursNumber': hoursNumber, 'description': description,
+            'document': document, 'storages': storages}
+    r = requests.post(
+        url=settings.url,
+        json=data
+    )
 
 
-# Pas besoin pour l'instant
-def get_post(post_id):
-    conn = get_db_connection()
-    post = conn.execute('SELECT * FROM posts WHERE id = ?',
-                        (post_id,)).fetchone()
-    conn.close()
-    if post is None:
-        abort(404)
-    return post
+@app.route('/index', methods='POST')
+def index():
+    if request.method == 'POST':
+        classes = request.form['classes']
+        teacherName = request.form['teacherName']
+        hoursNumber = request.form['hoursNumber']
+        description = request.form['description']
+        document = request.form['document']
+        storages = request.form['storages']
 
+        if not classes or storages:
+            flash('Un nom de cours et un type de stockage est requis est requis')
+        else:
+            post_rds(classes, teacherName, hoursNumber, description, document, storages)
+            return redirect(url_for('index'))
 
-@app.route('/create', methods=('GET', 'POST'))
-def create():
-    return render_template('create.html')
+    return render_template('index.html')
